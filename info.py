c53a8568093987844d70f2b68314de7fcb159aea
Nahidctg <164381234+Nahidctg@users.noreply.github.com>
2024-11-20 19:43:29 +0600

    Update info.py

--- info.py
+++ info.py
@@ -14,14 +14,14 @@ def is_enabled(value, default):
         return default
 
 
-ADMIN = int(getenv("ADMIN", ""))
-API_ID = int(getenv("API_ID", ""))
-API_HASH = str(getenv("API_HASH", ""))
-BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
+ADMIN = int(getenv("ADMIN", "7528643689"))
+API_ID = int(getenv("API_ID", "28870226"))
+API_HASH = str(getenv("API_HASH", "a5b1ff3f75941649bf5bc159782f0f00"))
+BOT_TOKEN = str(getenv("BOT_TOKEN", "7899900845:AAHEJZdghS15fvXPjWZf5t9chu65LpvtLm0"))
 MONGO_DB = str(
     getenv(
         "MONGO_DB",
-        "mongodb+srv://replacewithyourmongodb:replacewithyourmongodb@cluster0.zi78j51.mongodb.net/?retryWrites=true&w=majority",
+        "mongodb+srv://cogana5793:cogana5793@cluster0.1uo0s.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
     )
 )
 DEF_CAP = str(
